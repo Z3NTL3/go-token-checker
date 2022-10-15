ab8c3047ca6ca67ce89b1a3ce41bfd8fc318f4de
Efdal <00312802@rijnijssel.nl>
2022-10-15 13:40:28 +0200

    10/15/2022 13:40:27

---
+++ go.mod
@@ -0,0 +1,5 @@
+module z3ntl3/token-checker
+
+go 1.19
+
+require golang.org/x/sync v0.0.0-20220929204114-8fcdb60fdcc0
---
+++ main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"runtime"
+	"strings"
+	"z3ntl3/token-checker/builder"
+
+	"golang.org/x/sync/errgroup"
+)
+
+type information struct {
+	req    *http.Request
+	client *http.Client
+	token  string
+	file   *os.File
+}
+
+const (
+	API string = "https://discord.com/api/v9/users/@me"
+)
+
+var goods int
+var good_tokens string
+
+func (client information) CheckToken() error {
+	t := client.token
+	c := client.client
+
+	req := client.req
+
+	req.Header.Add("user-agent", "Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36")
+	req.Header.Add("Authorization", t)
+
+	content := make([]byte, 5000000)
+	resp, _ := c.Do(req)
+	length, _ := resp.Body.Read(content)
+
+	if !strings.Contains(string(content[0:length]), `Unauthorized`) {
+		fmt.Println("\033[1m\033[0;97m[INFO] \033[32mGood Token: \033[1m\033[0;97m", t, "\033[0m")
+		goods += 1
+		good_tokens += t
+
+		client.file.WriteString(t)
+	} else {
+		fmt.Println("\033[1m\033[0;97m[INFO] \033[31mBad Token: \033[1m\033[0;97m", t, "\033[0m")
+	}
+	return nil
+}
+
+func main() {
+	logo := builder.LogoBuild()
+	args := os.Args
+	max_worker_count := runtime.NumCPU()
+	free_cores := 3
+
+	workers := new(errgroup.Group)
+	workers.SetLimit(10000 * (max_worker_count - free_cores))
+
+	fmt.Printf(logo)
+	if len(args) != 2 {
+		fmt.Println(builder.Usage(), "\n")
+		return
+	}
+	content, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	tokens := strings.Split(string(content), "\n")
+	fmt.Println()
+
+	f, err := os.OpenFile("goods.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, v := range tokens {
+		v = strings.TrimSpace(v)
+		client := http.Client{}
+		request, err := http.NewRequest(http.MethodGet, API, nil)
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			info := information{
+				req:    request,
+				client: &client,
+				token:  v,
+				file:   f,
+			}
+
+			workers.Go(func() error {
+				return info.CheckToken()
+			})
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+	if err := workers.Wait(); err != nil {
+		fmt.Println(err)
+	}
+}
